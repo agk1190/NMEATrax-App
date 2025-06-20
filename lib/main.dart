0026b7b28d662068a0acb7688aaddc6b95b4c181
Alex K <90426659+agk1190@users.noreply.github.com>
2025-06-20 15:24:28 -0700

    commit version 6.1.0

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   // https://www.kindacode.com/article/flutter-ways-to-make-a-dark-light-mode-toggle/ - dark mode toggle
   static final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.dark);
-  static const appVersion = '6.0.0';
+  static const appVersion = '6.1.0';
 
   // This widget is the root of your application.
   @override

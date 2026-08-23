d52b62e89188984207749e23d4b91cf183895f42
Alex K <90426659+agk1190@users.noreply.github.com>
2026-08-22 19:52:16 -0700

    update versioning

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
 
 android {
     namespace = "com.nmeatrax.app"
-    compileSdk = flutter.compileSdkVersion
+    //compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -18,8 +19,7 @@ android {
         applicationId = "com.nmeatrax.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        // minSdk = flutter.minSdkVersion
-        minSdk = 37
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
